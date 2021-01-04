1a7a4925c43419accc423ee9da53b07312849a0c
Blaire Baker <bakerblaire@gmail.com>
2021-01-04 11:39:45 -0600

    created routes and view pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cars
+  resources :mechanics
+  resources :owners
+  resources :parts
+  resources :service_records
+  resources :services
 end
