3ae2e63aa452d2dde145a208b2b37e5c406d0ef4
Blaire Baker <bakerblaire@gmail.com>
2021-01-06 16:57:28 -0600

    end of day wednesday

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def errors
+        if flash[:errors]
+            flash[:errors].each do |error|
+                error 
+            end 
+        end
+    end
+
 end
