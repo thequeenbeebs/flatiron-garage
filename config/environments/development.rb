4fd661fae46bf72738aaea2a79b01a413c6bb0ac
logicfirst <slee@logicfirstllc.com>
2021-01-06 15:18:20 -0500

    mailer working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = {host: host}
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:            'scotthlee.mail@gmail.com',
+  password:             'rfqskrowrexjkysa',
+  authentication:       'plain',
+  enable_starttls_auto: true }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
